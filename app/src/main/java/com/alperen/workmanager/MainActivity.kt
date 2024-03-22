5f404284eced71e1e1a39f6e84b21380a7f5d2f2
AlpErenKaya0 <80575610+AlpErenKaya0@users.noreply.github.com>
2024-03-22 12:47:05 +0300

    commit Observer

--- app/src/main/java/com/alperen/workmanager/MainActivity.kt
+++ app/src/main/java/com/alperen/workmanager/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.alperen.workmanager
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.lifecycle.Observer
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
 import java.util.concurrent.TimeUnit
@@ -16,9 +19,45 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
             val newWorkManagerClass: WorkRequest = OneTimeWorkRequestBuilder<NewWorkManagerClass>()
-                .setInitialDelay(60, TimeUnit.SECONDS)
+                .setInitialDelay(3, TimeUnit.SECONDS)
                 .build()
+            //aşağıdaki instance satırı Observer'daki ENQUEUED'da güncel işlem varsa işlem yapılmasını engelleyecek
+            // ve isFinished durumunda veya WorkManager'ın iptal edilmesi durumunda instance'ının yeniden alınmasını sağlayacak
+            //yapı kurulabilir
             WorkManager.getInstance(this).enqueue(newWorkManagerClass)
+
+            WorkManager.getInstance(this).getWorkInfoByIdLiveData(newWorkManagerClass.id).observe(this,
+                Observer {workInfo->
+                    if (workInfo != null && workInfo.state.isFinished) {
+                        println("TESTtest")
+
+
+                    }
+                    else if (workInfo != null && workInfo.state == WorkInfo.State.ENQUEUED){
+                        println("enqueue")
+
+
+                    }
+                })
+/*
+            WorkManager.getInstance(this).getWorkInfoByIdLiveData(newWorkManagerClass.id).observe(this, Observer { workInfo ->
+                if (workInfo != null && workInfo.state == WorkInfo.State.ENQUEUED) {
+                    WorkManager.getInstance(this).enqueue(newWorkManagerClass)
+                    println("WorkManager task is already enqueued")
+                } else if (workInfo != null && workInfo.state == WorkInfo.State.FAILED) {
+                    println("WorkManager task failed :/")
+                    WorkManager.getInstance(this).enqueue(newWorkManagerClass)
+                } else if (workInfo != null && workInfo.state == WorkInfo.State.RUNNING) {
+                    println("WorkManager task is still running")
+                    WorkManager.getInstance(this).enqueue(newWorkManagerClass)
+                }
+                else if (workInfo!=null &&workInfo.state.isFinished){
+                    // Yeni bir istek sıraya alınacak çünkü önceki işlem tamamlanmış veya başlamamış
+                    WorkManager.getInstance(this).enqueue(newWorkManagerClass)
+                }
+
+            })
+*/
         }
         val button2 = findViewById<Button>(R.id.button2)
         button2.setOnClickListener {
