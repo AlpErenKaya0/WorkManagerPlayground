5b78f055cb3f237f5b24ebb8c28d62214b73cf0d
AlpErenKaya0 <80575610+AlpErenKaya0@users.noreply.github.com>
2024-03-22 14:07:23 +0300

    yeni WM oluşmasını engelleme ( her seferinde if çalıştırır )

--- app/src/main/java/com/alperen/workmanager/MainActivity.kt
+++ app/src/main/java/com/alperen/workmanager/MainActivity.kt
@@ -16,31 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         WorkManager.getInstance(this).cancelAllWork()
-
+        val newWorkManagerClass = OneTimeWorkRequestBuilder<NewWorkManagerClass>()
+            .setInitialDelay(10, TimeUnit.SECONDS)
+            .build()
+        WorkManager.getInstance(this).enqueue(newWorkManagerClass)
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-
-
-
-/*
-            WorkManager.getInstance(this).getWorkInfoByIdLiveData(newWorkManagerClass.id).observe(this, Observer { workInfo ->
-                if (workInfo != null && workInfo.state == WorkInfo.State.ENQUEUED) {
-                    WorkManager.getInstance(this).enqueue(newWorkManagerClass)
-                    println("WorkManager task is already enqueued")
-                } else if (workInfo != null && workInfo.state == WorkInfo.State.FAILED) {
-                    println("WorkManager task failed :/")
-                    WorkManager.getInstance(this).enqueue(newWorkManagerClass)
-                } else if (workInfo != null && workInfo.state == WorkInfo.State.RUNNING) {
-                    println("WorkManager task is still running")
-                    WorkManager.getInstance(this).enqueue(newWorkManagerClass)
-                }
-                else if (workInfo!=null &&workInfo.state.isFinished){
-                    // Yeni bir istek sıraya alınacak çünkü önceki işlem tamamlanmış veya başlamamış
-                    WorkManager.getInstance(this).enqueue(newWorkManagerClass)
-                }
-
-            })
-*/
+            WorkManager.getInstance(this).getWorkInfoByIdLiveData(newWorkManagerClass.id)
+                .observe(this, Observer { workInfo ->
+                    if (workInfo != null && workInfo.state == WorkInfo.State.ENQUEUED) {
+                        // Kuyrukta hali hazırda bir işlem var, yeni işlem oluşturmayın
+                        println("WorkManager task is already enqueued")
+                    } else if (workInfo != null && workInfo.state == WorkInfo.State.FAILED) {
+                        // İşlem başarısız oldu, yeni bir işlem oluşturabilirsiniz
+                        println("WorkManager task failed :/")
+                        WorkManager.getInstance(this).enqueue(newWorkManagerClass)
+                    } else if (workInfo != null && workInfo.state == WorkInfo.State.RUNNING) {
+                        // İşlem hala çalışıyor, yeni bir işlem oluşturmayın
+                        println("WorkManager task is still running")
+                    } else if (workInfo != null && workInfo.state.isFinished) {
+                        // Önceki işlem tamamlandı veya başlamadı, yeni bir işlem oluşturabilirsiniz
+                        WorkManager.getInstance(this).enqueue(newWorkManagerClass)
+                    }
+                })
         }
         val button2 = findViewById<Button>(R.id.button2)
         button2.setOnClickListener {
@@ -49,38 +47,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        val newWorkManagerClass: WorkRequest = OneTimeWorkRequestBuilder<NewWorkManagerClass>()
-            .setInitialDelay(10, TimeUnit.SECONDS)
-            .build()
-        //aşağıdaki instance satırı Observer'daki ENQUEUED'da güncel işlem varsa işlem yapılmasını engelleyecek
-        // ve isFinished durumunda veya WorkManager'ın iptal edilmesi durumunda instance'ının yeniden alınmasını sağlayacak
-        //yapı kurulabilir
-        WorkManager.getInstance(this).enqueue(newWorkManagerClass)
-        WorkManager.getInstance(this).getWorkInfoByIdLiveData(newWorkManagerClass.id).observe(this,
-            Observer {workInfo->
-                if (workInfo != null && workInfo.state.isFinished) {
-                    println("TESTtest")
-                    // aşağıdaki yemedi
-                    /*
-                WorkManager.getInstance(this).enqueue(newWorkManagerClass)
-                     */
-//bu çalışıyor ama her seferinde onCreate'de başlatılan gibi bunun için de ayrı kontrolör başlatmamız gerekir, olmaz
 
-                    val newWorkManagerClass = OneTimeWorkRequestBuilder<NewWorkManagerClass>()
-                        .setInitialDelay(10, TimeUnit.SECONDS)
-                        .build()
-                    WorkManager.getInstance(this).enqueue(newWorkManagerClass)
-
-
-
-                }
-                else if (workInfo != null && workInfo.state == WorkInfo.State.ENQUEUED){
-                    println("enqueue")
-
-
-                }
-            })
     }
-}
+
